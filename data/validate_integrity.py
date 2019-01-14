7c9d0208b39dbb528a8b1d60cce6780156a0846c
William A. Holley IV <will@vivism.com>
2019-01-13 23:41:17 -0500

    Moves image integrity checker into /data directory

---
+++ data/validate_integrity.py
@@ -0,0 +1,25 @@
+'''
+Script to check whether images are corrupted. Without an argument, checks `data/wikiart/`. Otherwise checks 
+the `data/<dataset-name>`
+Usage: 
+    python check_images.py <dataset-name>
+    
+    <dataset-name> : `data/<dataset-name>`
+'''
+from src.utils import test_images
+from glob import glob
+import sys
+
+try:
+	dataset = str(sys.argv[1])
+except IndexError:
+	dataset = "wikiart"
+
+path = "./{}/*/*.jpg".format(dataset)
+found = glob(path)
+count = len(found)
+
+print("{} images found -- validating their integrity...".format(count))
+
+test_images(found)
+

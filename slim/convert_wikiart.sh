a127705bc7c4ad902a0b43fbd1ec1402296e7b94
William A. Holley IV <will@vivism.com>
2019-01-21 16:28:58 -0500

    Removes Slim

--- slim/convert_wikiart.sh
+++
@@ -1,5 +0,0 @@
-export CUDA_VISIBLE_DEVICES=1
-python3 download_and_convert_data.py  \
-    --dataset_name "wikiart" \
-    --dataset_dir "/data/wikiart-records" \
-    --input_dataset_dir "/data/wikiart/"

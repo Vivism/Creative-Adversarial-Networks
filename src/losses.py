608b02b5d298c04f626bad3b1c7000855c232f8e
William A. Holley IV <will@vivism.com>
2019-02-01 18:27:17 -0500

    documenting

--- src/losses.py
+++ src/losses.py
@@ -2,71 +2,110 @@ import tensorflow as tf
 from ops import *
 
 def CAN_loss(model):
-    #builds optimizers and losses
+    
+    # builds optimizers and losses
+    model.G = model.generator(model, model.z)
+    model.D, model.D_logits, model.D_c, model.D_c_logits = model.discriminator(
+        model,
+        model.inputs,
+        reuse=False)
 
-    model.G                  = model.generator(model, model.z)
-    model.D, model.D_logits, model.D_c, model.D_c_logits     = model.discriminator(model,
-                                                              model.inputs, reuse=False)
     if model.experience_flag:
-      try:
-        model.experience_selection = tf.convert_to_tensor(random.sample(model.experience_buffer, 16))
-      except ValueError:
-        model.experience_selection = tf.convert_to_tensor(model.experience_buffer)
-      model.G = tf.concat([model.G, model.experience_selection], axis=0)
+        try:
+            # Select a random subset of 16 elements
+            random_elements = random.sample(model.experience_buffer, 16)
+            experience_selection = tf.convert_to_tensor(random_elements)
+        except ValueError:
+            experience_selection = tf.convert_to_tensor(model.experience_buffer)
+        
+        # Combine the experience selection 
+        model.G = tf.concat([model.G, experience_selection], axis=0)
 
-    model.D_, model.D_logits_, model.D_c_, model.D_c_logits_ = model.discriminator(model,
-                                                              model.G, reuse=True)
+    model.D_, model.D_logits_, model.D_c_, model.D_c_logits_ = model.discriminator(model, model.G, reuse=True)
     model.d_sum = histogram_summary("d", model.D)
     model.d__sum = histogram_summary("d_", model.D_)
     model.d_c_sum = histogram_summary("d_c", model.D_c)
     model.d_c__sum = histogram_summary("d_c_", model.D_c_)
     model.G_sum = image_summary("G", model.G)
 
-    correct_prediction = tf.equal(tf.argmax(model.y,1), tf.argmax(model.D_c,1))
-    model.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    max_y = tf.argmax(input=model.y, axis=1)
+    max_d_c = tf.argmax(input=model.D_c, axis=1)
+    correct_prediction = tf.equal(x=max_y, y=max_d_c)
 
-    true_label = tf.random_uniform(tf.shape(model.D),.8, 1.2)
-    false_label = tf.random_uniform(tf.shape(model.D_), 0.0, 0.3)
+    prediction_to_float = tf.cast(x=correct_prediction, dtype=tf.float32)
+    model.accuracy = tf.reduce_mean(input_tensor=prediction_to_float)
 
-    model.d_loss_real = tf.reduce_mean(
-      sigmoid_cross_entropy_with_logits(model.D_logits, true_label * tf.ones_like(model.D)))
+    true_label = tf.random_uniform(
+        shape=tf.shape(model.D),
+        minval=.8,
+        maxval=1.2
+    )
 
-    model.d_loss_fake = tf.reduce_mean(
-      sigmoid_cross_entropy_with_logits(model.D_logits_, false_label * tf.ones_like(model.D_)))
+    false_label = tf.random_uniform(
+        shape=tf.shape(model.D_),
+        minval=0.0,
+        maxval=0.3
+    )
 
-    model.d_loss_class_real = tf.reduce_mean(
-      tf.nn.softmax_cross_entropy_with_logits(logits=model.D_c_logits, labels=model.smoothing * model.y))
+    tensor = sigmoid_cross_entropy_with_logits(
+        x=model.D_logits,
+        y=true_label * tf.ones_like(model.D)
+    )
+    model.d_loss_real = tf.reduce_mean(input_tensor=tensor)
 
-    # if classifier is set, then use the classifier, o/w use the clasification layers in the discriminator
+    tensor = sigmoid_cross_entropy_with_logits(
+        x=model.D_logits_,
+        y=false_label * tf.ones_like(model.D_)
+    )
+    model.d_loss_fake = tf.reduce_mean(input_tensor=tensor)
+
+    tensor = tf.nn.softmax_cross_entropy_with_logits(logits=model.D_c_logits, labels=model.smoothing * model.y)
+    model.d_loss_class_real = tf.reduce_mean(input_tensor=tensor)
+
+    # if classifier is set, then use the classifier,
+    # otherwise use the clasification layers in the discriminator
+    ls = ( 1.0 / model.y_dim ) * tf.ones_like(model.D_c_)
     if model.style_net_checkpoint is None:
-      model.g_loss_class_fake = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(logits=model.D_c_logits_,
-          labels=(1.0/model.y_dim)*tf.ones_like(model.D_c_)))
+        tensor = tf.nn.softmax_cross_entropy_with_logits(logits=model.D_c_logits_, ls=ls)
     else:
-      model.classifier = model.make_style_net(model.G)
-      model.g_loss_class_fake = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(logits=model.classifier,
-          labels=(1.0/model.y_dim)*tf.ones_like(model.D_c_)))
+        model.classifier = model.make_style_net(model.G)
+        tensor = tf.nn.softmax_cross_entropy_with_logits(logits=model.classifier, labels=ls)
+    
+    model.g_loss_class_fake = tf.reduce_mean(input_tensor=tensor)
 
     model.g_loss_fake = -tf.reduce_mean(tf.log(model.D_))
 
     model.d_loss = model.d_loss_real + model.d_loss_class_real + model.d_loss_fake
     model.g_loss = model.g_loss_fake + model.lamb * model.g_loss_class_fake
 
-    model.d_loss_real_sum       = scalar_summary("d_loss_real", model.d_loss_real)
-    model.d_loss_fake_sum       = scalar_summary("d_loss_fake", model.d_loss_fake)
+    model.d_loss_real_sum = scalar_summary("d_loss_real", model.d_loss_real)
+    model.d_loss_fake_sum = scalar_summary("d_loss_fake", model.d_loss_fake)
     model.d_loss_class_real_sum = scalar_summary("d_loss_class_real", model.d_loss_class_real)
     model.g_loss_class_fake_sum = scalar_summary("g_loss_class_fake", model.g_loss_class_fake)
     model.g_loss_sum = scalar_summary("g_loss", model.g_loss)
     model.d_loss_sum = scalar_summary("d_loss", model.d_loss)
-    model.d_sum = merge_summary(
-        [model.z_sum, model.d_sum, model.d_loss_real_sum, model.d_loss_sum,
-        model.d_loss_class_real_sum, model.g_loss_class_fake_sum])
-    model.g_sum = merge_summary([model.z_sum, model.d__sum,
-      model.G_sum, model.d_loss_fake_sum, model.g_loss_sum])
+    model.d_sum = merge_summary([
+        model.z_sum,
+        model.d_sum,
+        model.d_loss_real_sum,
+        model.d_loss_sum,
+        model.d_loss_class_real_sum,
+        model.g_loss_class_fake_sum
+    ])
+    model.g_sum = merge_summary([
+        model.z_sum,
+        model.d__sum,
+        model.G_sum,
+        model.d_loss_fake_sum,
+        model.g_loss_sum
+    ])
 
-    model.g_opt = tf.train.AdamOptimizer(learning_rate=model.learning_rate, beta1=0.5)
-    model.d_opt = tf.train.AdamOptimizer(learning_rate=model.learning_rate, beta1=0.5)
+    adam_args = {
+        "learning_rate": model.learning_rate,
+        "beta1": 0.5
+    }
+    model.g_opt = tf.train.AdamOptimizer(**adam_args)
+    model.d_opt = tf.train.AdamOptimizer(**adam_args)
 
     t_vars = tf.trainable_variables()
     d_vars = [var for var in t_vars if 'd_' in var.name]
@@ -82,21 +121,18 @@ def WCAN_loss(model):
 
 
 def GAN_loss(model):
-    model.G                  = model.generator(model.z, model.y)
-    model.D, model.D_logits   = model.discriminator(model.inputs, model.y, reuse=False)
+    model.G = model.generator(model.z, model.y)
+    model.D, model.D_logits = model.discriminator(model.inputs, model.y, reuse=False)
     model.D_, model.D_logits_ = model.discriminator(model.G, model.y, reuse=True)
 
-    true_label = tf.random_uniform(tf.shape(model.D),.8, 1.2)
+    true_label = tf.random_uniform(tf.shape(model.D), .8, 1.2)
     false_label = tf.random_uniform(tf.shape(model.D_), 0.0, 0.3)
 
-    model.d_loss_real = tf.reduce_mean(
-      sigmoid_cross_entropy_with_logits(model.D_logits, true_label * tf.ones_like(model.D)))
+    model.d_loss_real = tf.reduce_mean(sigmoid_cross_entropy_with_logits(model.D_logits, true_label * tf.ones_like(model.D)))
 
-    model.d_loss_fake = tf.reduce_mean(
-            sigmoid_cross_entropy_with_logits(model.D_logits_, false_label * tf.ones_like(model.D_)))
+    model.d_loss_fake = tf.reduce_mean(sigmoid_cross_entropy_with_logits(model.D_logits_, false_label * tf.ones_like(model.D_)))
 
-    model.g_loss = tf.reduce_mean(
-      sigmoid_cross_entropy_with_logits(model.D_logits_, tf.ones_like(model.D_)))
+    model.g_loss = tf.reduce_mean(sigmoid_cross_entropy_with_logits(model.D_logits_, tf.ones_like(model.D_)))
     model.d_loss = model.d_loss_real + model.d_loss_fake
 
     model.d_sum = histogram_summary("d", model.D)
@@ -107,10 +143,8 @@ def GAN_loss(model):
     model.d_loss_sum = scalar_summary("d_loss", model.d_loss)
     model.d_loss_real_sum = scalar_summary("d_loss_real", model.d_loss_real)
     model.d_loss_fake_sum = scalar_summary("d_loss_fake", model.d_loss_fake)
-    model.d_sum = merge_summary(
-      [model.z_sum, model.d_sum, model.d_loss_real_sum, model.d_loss_sum])
-    model.g_sum = merge_summary([model.z_sum, model.d__sum,
-      model.G_sum, model.d_loss_fake_sum, model.g_loss_sum])
+    model.d_sum = merge_summary([model.z_sum, model.d_sum, model.d_loss_real_sum, model.d_loss_sum])
+    model.g_sum = merge_summary([model.z_sum, model.d__sum, model.G_sum, model.d_loss_fake_sum, model.g_loss_sum])
 
     model.g_opt = tf.train.AdamOptimizer(learning_rate=model.learning_rate, beta1=0.5)
     model.d_opt = tf.train.AdamOptimizer(learning_rate=model.learning_rate, beta1=0.5)
@@ -161,5 +195,5 @@ def WGAN_loss(model):
 
     model.d_sum = merge_summary([model.d_loss_sum, model.wp_sum, model.gp_sum])
     model.g_sum = merge_summary([model.g_loss_sum, model.G_sum])
+    
     return d_update, g_update, loss_ops, [model.d_sum, model.g_sum]
-

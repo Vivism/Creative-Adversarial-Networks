40aead07a87e7596be28bf8478482cf0b8408249
William A. Holley IV <will@vivism.com>
2019-02-02 11:48:04 -0500

    2 to 4 spaces

--- src/main.py
+++ src/main.py
@@ -47,117 +47,117 @@ flags.DEFINE_boolean("allow_gpu_growth", False, "True if you want Tensorflow onl
 FLAGS = flags.FLAGS
 
 def main(_):
-  print('\nProcessing Flags\n')
-  pp.pprint(flags.FLAGS.__flags)
+    print('\nProcessing Flags\n')
+    pp.pprint(flags.FLAGS.__flags)
   
-  # check to see if we want to use AWS S3
-  if FLAGS.use_s3:
-    if FLAGS.s3_bucket is None:
-      raise ValueError('use_s3 flag set, but no bucket set. ')
-    # check to see if s3 bucket exists:
-    elif not aws.bucket_exists(FLAGS.s3_bucket):
-      raise ValueError('`use_s3` flag set, but bucket "%s" doesn\'t exist. Not using s3' % FLAGS.s3_bucket)
+    # check to see if we want to use AWS S3
+    if FLAGS.use_s3:
+        if FLAGS.s3_bucket is None:
+            raise ValueError('use_s3 flag set, but no bucket set. ')
+        # check to see if s3 bucket exists:
+        elif not aws.bucket_exists(FLAGS.s3_bucket):
+            raise ValueError('`use_s3` flag set, but bucket "%s" doesn\'t exist. Not using s3' % FLAGS.s3_bucket)
 
-  # set dimensions
-  if FLAGS.input_width is None:
-    FLAGS.input_width = FLAGS.input_height
-  if FLAGS.output_width is None:
-    FLAGS.output_width = FLAGS.output_height
+    # set dimensions
+    if FLAGS.input_width is None:
+        FLAGS.input_width = FLAGS.input_height
+    if FLAGS.output_width is None:
+        FLAGS.output_width = FLAGS.output_height
 
-  # configure the log_dir to match the params
-  dir_string = "dataset={},isCan={},lr={},imsize={},hasStyleNet={},batch_size={}".format(
-    FLAGS.dataset,
-    FLAGS.can,
-    FLAGS.learning_rate,
-    FLAGS.input_height,
-    FLAGS.style_net_checkpoint is not None,
-    FLAGS.batch_size
-  )
-  log_dir = os.path.join(FLAGS.log_dir, dir_string)
+    # configure the log_dir to match the params
+    dir_string = "dataset={},isCan={},lr={},imsize={},hasStyleNet={},batch_size={}".format(
+        FLAGS.dataset,
+        FLAGS.can,
+        FLAGS.learning_rate,
+        FLAGS.input_height,
+        FLAGS.style_net_checkpoint is not None,
+        FLAGS.batch_size
+    )
+    log_dir = os.path.join(FLAGS.log_dir, dir_string)
 
-  # Set log directory
-  if not glob(log_dir + "*"):
-    log_dir = os.path.join(log_dir, "000")
-  else:
-    containing_dir = os.path.join(log_dir, "[0-9][0-9][0-9]")
-    nums = [int(x[-3:]) for x in glob(containing_dir)] # TODO FIX THESE HACKS
-    if nums == []:
-      num = 0
+    # Set log directory
+    if not glob(log_dir + "*"):
+        log_dir = os.path.join(log_dir, "000")
     else:
-      num = max(nums) + 1
-    log_dir = os.path.join(log_dir,"{:03d}".format(num))
-  FLAGS.log_dir = log_dir
+        containing_dir = os.path.join(log_dir, "[0-9][0-9][0-9]")
+        nums = [int(x[-3:]) for x in glob(containing_dir)] # TODO FIX THESE HACKS
+        if nums == []:
+            num = 0
+        else:
+            num = max(nums) + 1
+        log_dir = os.path.join(log_dir,"{:03d}".format(num))
+    FLAGS.log_dir = log_dir
 
-  # set checkpoint directory
-  if FLAGS.checkpoint_dir is None:
-    FLAGS.checkpoint_dir = os.path.join(FLAGS.log_dir, 'checkpoint')
-    FLAGS.use_default_checkpoint = True
-  elif FLAGS.use_default_checkpoint:
-    raise ValueError('`use_default_checkpoint` flag only works if you keep checkpoint_dir as None')
+    # set checkpoint directory
+    if FLAGS.checkpoint_dir is None:
+        FLAGS.checkpoint_dir = os.path.join(FLAGS.log_dir, 'checkpoint')
+        FLAGS.use_default_checkpoint = True
+    elif FLAGS.use_default_checkpoint:
+        raise ValueError('`use_default_checkpoint` flag only works if you keep checkpoint_dir as None')
 
-  if FLAGS.sample_dir is None:
-    FLAGS.sample_dir = os.path.join(FLAGS.log_dir, 'samples')
+    if FLAGS.sample_dir is None:
+        FLAGS.sample_dir = os.path.join(FLAGS.log_dir, 'samples')
 
-  if not os.path.exists(FLAGS.checkpoint_dir):
-    os.makedirs(FLAGS.checkpoint_dir)
+    if not os.path.exists(FLAGS.checkpoint_dir):
+        os.makedirs(FLAGS.checkpoint_dir)
 
-  if not os.path.exists(FLAGS.sample_dir):
-    os.makedirs(FLAGS.sample_dir)
+    if not os.path.exists(FLAGS.sample_dir):
+        os.makedirs(FLAGS.sample_dir)
 
-  print('\nFlags Processed\n')
-  pp.pprint(flags.FLAGS.__flags)
+    print('\nFlags Processed\n')
+    pp.pprint(flags.FLAGS.__flags)
 
-  if FLAGS.style_net_checkpoint:
-    network_fn = nets_factory
+    if FLAGS.style_net_checkpoint:
+        network_fn = nets_factory
 
-  # setup DCGAN
-  sess = None
-  # set ydim
-  if FLAGS.dataset == 'mnist':
-    y_dim = 10
-  elif FLAGS.dataset == 'wikiart':
-    y_dim = 27
-  else:
-    y_dim = None
+    # setup DCGAN
+    sess = None
+    # set ydim
+    if FLAGS.dataset == 'mnist':
+        y_dim = 10
+    elif FLAGS.dataset == 'wikiart':
+        y_dim = 27
+    else:
+        y_dim = None
 
-  dcgan = DCGAN(
-    sess,
-    input_width=FLAGS.input_width,
-    input_height=FLAGS.input_height,
-    output_width=FLAGS.output_width,
-    output_height=FLAGS.output_height,
-    batch_size=FLAGS.batch_size,
-    sample_num=FLAGS.sample_size,
-    use_resize=FLAGS.use_resize,
-    replay=FLAGS.replay,
-    y_dim=y_dim,
-    smoothing=FLAGS.smoothing,
-    lamb = FLAGS.lambda_val,
-    dataset_name=FLAGS.dataset,
-    input_fname_pattern=FLAGS.input_fname_pattern,
-    crop=FLAGS.crop,
-    checkpoint_dir=FLAGS.checkpoint_dir,
-    sample_dir=FLAGS.sample_dir,
-    wgan=FLAGS.wgan,
-    learning_rate = FLAGS.learning_rate,
-    style_net_checkpoint=FLAGS.style_net_checkpoint,
-    can=FLAGS.can
-  )
+    dcgan = DCGAN(
+        sess,
+        input_width=FLAGS.input_width,
+        input_height=FLAGS.input_height,
+        output_width=FLAGS.output_width,
+        output_height=FLAGS.output_height,
+        batch_size=FLAGS.batch_size,
+        sample_num=FLAGS.sample_size,
+        use_resize=FLAGS.use_resize,
+        replay=FLAGS.replay,
+        y_dim=y_dim,
+        smoothing=FLAGS.smoothing,
+        lamb = FLAGS.lambda_val,
+        dataset_name=FLAGS.dataset,
+        input_fname_pattern=FLAGS.input_fname_pattern,
+        crop=FLAGS.crop,
+        checkpoint_dir=FLAGS.checkpoint_dir,
+        sample_dir=FLAGS.sample_dir,
+        wgan=FLAGS.wgan,
+        learning_rate = FLAGS.learning_rate,
+        style_net_checkpoint=FLAGS.style_net_checkpoint,
+        can=FLAGS.can
+    )
 
-  run_config = tf.ConfigProto()
-  run_config.gpu_options.allow_growth=FLAGS.allow_gpu_growth
-  with tf.Session(config=run_config) as sess:
-    dcgan.set_sess(sess)
-    # show_all_variables()
+    run_config = tf.ConfigProto()
+    run_config.gpu_options.allow_growth=FLAGS.allow_gpu_growth
+    with tf.Session(config=run_config) as sess:
+        dcgan.set_sess(sess)
+        # show_all_variables()
 
-    if FLAGS.train:
-      dcgan.train(FLAGS)
-    # else:
-    #   if not dcgan.load(FLAGS.checkpoint_dir)[0]:
-    #     raise Exception("[!] Train a model first, then run test mode")
+        if FLAGS.train:
+            dcgan.train(FLAGS)
+        # else:
+        #   if not dcgan.load(FLAGS.checkpoint_dir)[0]:
+        #     raise Exception("[!] Train a model first, then run test mode")
 
-    OPTION = 0
-    visualize(sess, dcgan, FLAGS, OPTION)
+        OPTION = 0
+        visualize(sess, dcgan, FLAGS, OPTION)
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
